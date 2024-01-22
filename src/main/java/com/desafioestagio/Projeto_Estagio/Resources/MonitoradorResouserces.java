2dba507e75bc271beba52a73e376344880e69ab1
Davii-code <davifariagoncalves@gmail.com>
2024-01-22 17:29:59 -0300

    "angular erro de CORS Resolvido"

--- src/main/java/com/desafioestagio/Projeto_Estagio/Resources/MonitoradorResouserces.java
+++ src/main/java/com/desafioestagio/Projeto_Estagio/Resources/MonitoradorResouserces.java
@@ -28,6 +28,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/monitorador", produces = "application/json; charset=UTF-8")
+@CrossOrigin(origins = "http://localhost:4200")
 public class MonitoradorResouserces {
 
     @Autowired
